ac323e558e4233581872b690b44da43b884e1eac
faberfff <faverfff@gmail.com>
2021-11-14 09:26:20 -0500

    primer commit

---
+++ database/migrations/2021_11_10_213435_create_post_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Faker\ORM\Spot\ColumnTypeGuesser;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id();
+            $table->string('publication')->nullable()->default('text');
+            $table->enum('state',['Publicado', 'Rechazado', 'Revisión', 'Recibido'])->nullable()->default('Recibido');
+            $table->text('publication_content')->nullable();
+            $table->bigInteger('category_id')->nullable()->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+
+     <h1>Contenido de home</h1>
+
+@endsection     
